2365860248387f177c34cb7813f1da7eb66a6342
Jagadish315 <jagadishbhandari315@gmail.com>
2020-09-24 13:19:08 -0400

    Product assignment

---
+++ constructorPack/ProductDemo.java
@@ -0,0 +1,15 @@
+package constructorPack;
+
+public class ProductDemo {
+
+	public static void main(String[] args) {
+	
+		Product p = new Product(200, "Dabar", 20.50, 4);
+		System.out.println(p);
+		Product p1= new Product(100, "Colagate", 5.00, 3);
+		System.out.println(p1);
+		
+
+	}
+
+}
